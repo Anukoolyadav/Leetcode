b78c0e59d87ea4d23cd2c2289b7d75b067526845
Anukool Yadav <anukoolyadav@Anukools-MacBook-Air.local>
2025-05-26 22:00:12 +0530

    Add RemoveDuplicate.cpp solution

---
+++ RemoveDuplicate.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<list>
+#include<bitset>
+#include<iomanip>
+#include<chrono>
+#include<random>
+#include<fstream>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+const ll MOD = 998244353;
+const int INTMOD = 1e9 + 7;
+const double eps = 1e-12;
+const ll INF = 2e18;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ll pow2[61];
+p64 dp[61][61][2];
+bool visited[61][61][2];
+v32 bits;
+ll K;
+void solve(string s){
+    vector<int> t(26,0);
+    vector<bool> chk(26,false);
+    forn(i,s.length()){
+        t[s[i]-'a']++;
+    }
+    string st;
+    forn(i,s.length()){
+        t[s[i]-'a']--;
+        if(chk[s[i]-'a']) continue;
+        while(!st.empty() && t[st.back()-'a']>0 && st.back()>s[i]){
+
+            
+
+            
+            chk[st.back()-'a']=false;
+            st.pop_back();
+        }
+        
+        st.push_back(s[i]);
+        chk[s[i]-'a']=true;
+    }
+    cout<<st;
+    
+}
+int main(){
+    string s;
+    cin>>s;
+    solve(s);
+    return 0;
+}
